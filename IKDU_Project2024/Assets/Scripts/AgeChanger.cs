2257427f4cc38ca27b57e76e98c633d5a777e3a4
MathiasPHA <Mathias.p.h.andersen@gmail.com>
2024-10-21 22:13:27 +0200

    Age changer completed

---
+++ IKDU_Project2024/Assets/Scripts/AgeChanger.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgeChanger : MonoBehaviour
+{
+    //Initializing variables
+    public int currentAge = 24;
+    public int addYears = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calling the function, calculating my age in 1 year.
+        futureAge(currentAge);
+        //displaying the results for the fuction in the console
+        Debug.LogFormat("My age next year is {0}", futureAge(currentAge));
+
+        //calling the function, calculating my age in n (addYears) years, n being 5. 
+        futureAge(currentAge, addYears);
+        //displaying the results for the fuction in the console
+        Debug.LogFormat("My age in {0} years is {1}", addYears, futureAge(currentAge, addYears));
+    }
+
+    //a function, returning my current age + 1.
+    int futureAge(int n) 
+    {
+        
+        return (currentAge + 1);
+    }
+
+    //a function returning my current age + n (addYears).
+    int futureAge(int n, int m)
+    {
+        return (currentAge + addYears);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
